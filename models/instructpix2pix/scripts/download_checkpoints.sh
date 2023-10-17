e24a570cc88122055b0e5e14fd12a5f8cc2bfb65
juxuan27 <juxuan.27@gmail.com>
2023-10-17 16:29:45 +0800

    init website

--- models/instructpix2pix/scripts/download_checkpoints.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-mkdir -p $SCRIPT_DIR/../checkpoints
-
-curl http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt -o $SCRIPT_DIR/../checkpoints/instruct-pix2pix-00-22000.ckpt
